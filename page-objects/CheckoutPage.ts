851ac3b47fb3ac5563e0c7a8940fd3827e2c1b49
pwcislo26 <crouchu26@gmail.com>
2024-08-12 18:39:41 +0200

    create checkout page object

---
+++ page-objects/CheckoutPage.ts
@@ -0,0 +1,27 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "./BasePage";
+
+export class CheckoutPage extends BasePage {
+  readonly checkoutPageUrl: string;
+  readonly checkoutPageSuccessUrl: string;
+  readonly confirmOrderButton: Locator;
+  readonly checkoutSuccessfullMessage: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.confirmOrderButton = this.page.getByRole("button", {
+      name: "Confirm Order",
+    });
+    this.checkoutPageUrl =
+      "https://automationteststore.com/index.php?rt=checkout/confirm";
+    this.checkoutPageSuccessUrl =
+      "https://automationteststore.com/index.php?rt=checkout/success";
+    this.checkoutSuccessfullMessage = this.page.getByText(
+      " Your Order Has Been Processed!"
+    );
+  }
+
+  async confirmOrder() {
+    this.confirmOrderButton.click();
+  }
+}
