15e2019ae42a31516a7b9f24658eea56d7753755
pwcislo26 <crouchu26@gmail.com>
2024-08-12 18:40:02 +0200

    add addFirstNProductsToCart method

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -1,15 +1,29 @@
-import { Page } from "@playwright/test";
+import { Page, Locator } from '@playwright/test';
 import { BasePage } from "./BasePage";
 
 export class HomePage extends BasePage {
   readonly homePageUrl: string;
+  readonly categoryMenu: Locator
+  readonly addToCartButton: Locator
 
   constructor(page: Page) {
     super(page);
     this.homePageUrl = "https://automationteststore.com/";
+    this.categoryMenu = this.page.locator('.nav-pills categorymenu')
+    this.addToCartButton = this.page.locator('a[title="Add to Cart"]');
   }
 
   async openHomePage() {
     await super.navigateTo(this.homePageUrl);
+  }
+
+  async addFirstNProductsToCart(numberOfProducts: number){
+    const buttonCount = await this.addToCartButton.count();
+    if (buttonCount < numberOfProducts){
+      throw new Error(`Request ${numberOfProducts} products, but only ${buttonCount} is available`)
+    }
+    for (let i = 0; i < numberOfProducts; i++){
+      await this.addToCartButton.nth(i).click();
+    }
   }
 }
