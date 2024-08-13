26457d42a2528997698e80b5576e28d7b8e4f6ea
pwcislo26 <crouchu26@gmail.com>
2024-08-13 20:01:32 +0200

    add browser context

--- fixtures/login.fixture.ts
+++ fixtures/login.fixture.ts
@@ -7,12 +7,18 @@ export type LoginFixtures = {
 };
 
 export const test = base.extend<LoginFixtures>({
+  page: async ({ browser }, use) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await use(page);
+    await context.close(); // Ensure context is closed after each test
+  },
   login: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
     const login = async (login: string, password: string) => {
-      await loginPage.openLoginPage()
+      await loginPage.openLoginPage();
       await loginPage.performLogin(login, password);
     };
     await use(login);
-  }
+  },
 });
